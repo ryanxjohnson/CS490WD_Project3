f64d442b86df44c1b2da48c0dd2b9b458f5ed7d4
ryanxjohnson <ryan.x.johnson@gmail.com>
2014-11-14 18:57:03 -0600

    code clean up

--- views/find_car.php
+++ views/find_car.php
@@ -3,11 +3,15 @@ require_once("/../classes/Car.php");
 
 $search_available_cars = new Car();
 $data="empty search yields all results";
+
 if (isset($_POST['search_field']) && trim($_POST['search_field']) != "") {
+    
     $data = $_POST['search_field'];
-    $query = $search_available_cars->get_cars_by_search($data);
+    
+    $query = $search_available_cars->get_cars_by_search($data);  
     
 } elseif (!isset($_POST['search_field']) || trim($_POST['search_field']) == "" || trim($_POST['search_field']) == null ) {
+    
     $query = $search_available_cars->get_available_cars();    
 }
 
